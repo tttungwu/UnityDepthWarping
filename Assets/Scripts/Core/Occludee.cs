250d57bdcf3072b0b5cfa99bd425f4046f35dfea
tungwu <tungwuwt@gmail.com>
2025-03-10 14:17:42 +0800

    patch

---
+++ Assets/Scripts/Core/Occludee.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Core
+{
+    [RequireComponent(typeof(Renderer))]
+    public class Occludee : MonoBehaviour
+    {
+        private int _visibleLayer;
+        private int _invisibleLayer;
+
+        private void Start()
+        {
+            _visibleLayer = LayerMask.NameToLayer("Default");
+            _invisibleLayer = LayerMask.NameToLayer("Occludee");
+            gameObject.layer = _visibleLayer;
+        }
+    
+        public void MarkAsOccluded()
+        {
+            gameObject.layer = _invisibleLayer;
+        }
+
+        public void MarkAsVisible()
+        {
+            gameObject.layer = _visibleLayer;
+        }
+    
+        public Bounds GetBounds()
+        {
+            return GetComponent<Renderer>().bounds;
+        }
+    }
+}
