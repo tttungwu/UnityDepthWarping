b27342c7c7f08ad44c5c39cce31fdf4def7c74bb
tttung <tungwuwt@gmail.com>
2025-03-13 23:18:38 +0800

    frustum culling

--- Assets/Scripts/Core/CullingMgr.cs
+++ Assets/Scripts/Core/CullingMgr.cs
@@ -21,7 +21,22 @@ public class CullingMgr : MonoBehaviour
 
     void Update()
     {
+        List<Occludee> occludees = FindObjectsOfType<Occludee>().ToList();
         foreach (var cullingMethod in cullingMethods)
-            cullingMethod.Cull();
+        {
+            cullingMethod.Cull(occludees);
+            occludees = cullingMethod.GetVisibleBounds();
+        }
+
+        List<Occludee> invisible = cullingMethods[0].GetInvisibleBounds();
+        for (int i = 0; i < invisible.Count; i++)
+        {
+            invisible[i].MarkAsOccluded();
+        }
+        List<Occludee> visible = cullingMethods[0].GetVisibleBounds();
+        for (int i = 0; i < visible.Count; i++)
+        {
+            visible[i].MarkAsVisible();
+        }
     }
 }
