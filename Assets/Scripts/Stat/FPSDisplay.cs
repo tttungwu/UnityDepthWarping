a6d028e36f86a68eef747e9ad3a0864853707be3
tungwu <tungwuwt@gmail.com>
2025-03-14 20:04:54 +0800

    UI

---
+++ Assets/Scripts/Stat/FPSDisplay.cs
@@ -0,0 +1,66 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace Stat
+{
+    public class FPSDisplay : MonoBehaviour
+    {
+        public TextMeshProUGUI uiText;
+        public int startFrameIdx;
+        public float accumTimeLimit = 60.0f;
+        public float deltaTimeUpdateFrequency = 0.1f;
+        private int _frameIdx;
+        private float _minDeltaTime = float.MaxValue;
+        private float _maxDeltaTime = float.MinValue;
+        private float _accumTime;
+        private float _displayDeltaTime;
+        private float _displayAccumTime;
+        private int _displayFrameCount;
+
+        private void Update()
+        {
+            if (_accumTime >= accumTimeLimit) return;
+// #if UNITY_ANDROID
+//         if (Time.deltaTime <= 1.0f / 144.0f) return; // exclude outlier  
+// #endif
+            _frameIdx++;
+            if (_frameIdx < startFrameIdx) return;
+        
+            _displayAccumTime += Time.deltaTime;
+            _displayFrameCount++;
+            if (_displayAccumTime >= deltaTimeUpdateFrequency)
+            {
+                _displayDeltaTime = _displayAccumTime / _displayFrameCount;
+                _displayAccumTime = 0;
+                _displayFrameCount = 0;
+            }
+            
+            _minDeltaTime = Math.Min(_minDeltaTime, Time.deltaTime);
+            _maxDeltaTime = Math.Max(_maxDeltaTime, Time.deltaTime);
+            _accumTime += Time.deltaTime;
+        }
+
+        private void OnGUI()
+        {
+            string text;
+            if (_frameIdx < startFrameIdx)
+            {
+                text = $"FrameCount: {_frameIdx}";
+            }
+            else
+            {
+                text = $"FPS: {1f / _displayDeltaTime:F3}" +
+                       $"\nDeltaTime: {_displayDeltaTime * 1000f:F3}" +
+                       $"\nMinFPS: {1f / _maxDeltaTime:F3}" +
+                       $"\nMaxFPS: {1f / _minDeltaTime:F3}" +
+                       $"\nAvgFPS: {(_frameIdx - startFrameIdx + 1) / _accumTime:F3}" +
+                       $"\nFrameCount: {_frameIdx - startFrameIdx + 1}" +
+                       $"\nAccumTime: {_accumTime:F3}";
+            }
+            uiText.text = text;
+            // GUI.Box(new Rect(100, 100, 200, 30), text);
+            // GUI.Box(new Rect(100, 100, 200, 130), text);
+        }
+    }
+}
