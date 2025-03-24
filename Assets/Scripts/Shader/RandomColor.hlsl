ef5f59344276f80d17e7f4f893d4c185f74fc955
tttung <tungwuwt@gmail.com>
2025-03-24 23:38:29 +0800

    shader graph

--- Assets/Scripts/Shader/RandomColor.hlsl
+++ Assets/Scripts/Shader/RandomColor.hlsl
@@ -1,36 +1,30 @@
-// #include <UnityShaderVariables.cginc>
+
 
 #if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-	StructuredBuffer<float4x4> instanceMatrix;
+StructuredBuffer<float4x4> instanceMatrices;
 #endif
-#include <UnityShaderVariables.cginc>
 
 void ConfigureProcedural ()
 {
 	#if defined(UNITY_PROCEDURAL_INSTANCING_ENABLED)
-		unity_ObjectToWorld = instanceMatrix[unity_InstanceID];
+	unity_ObjectToWorld = instanceMatrices[unity_InstanceID];
 	#endif
 }
 
-float4 GetColor ()
+
+float4 GetFractalColor ()
 {
-	// float3 color = float3(
-	// 	frac((unity_ObjectToWorld._11 + unity_ObjectToWorld._12 + unity_ObjectToWorld._13 + unity_ObjectToWorld._14) / 4.0f),
-	// 	frac((unity_ObjectToWorld._21 + unity_ObjectToWorld._22 + unity_ObjectToWorld._23 + unity_ObjectToWorld._24) / 4.0f),
-	// 	frac((unity_ObjectToWorld._31 + unity_ObjectToWorld._32 + unity_ObjectToWorld._33 + unity_ObjectToWorld._34) / 4.0f)
-	// );
-	// return float4(color, 1.0);
 	return float4(1.0f, 0.0f, 0.0f, 1.0f);
 }
 
 void ShaderGraphFunction_float (float3 In, out float3 Out, out float4 InstanceColor)
 {
 	Out = In;
-	InstanceColor = GetColor();
+	InstanceColor = GetFractalColor();
 }
 
 void ShaderGraphFunction_half (half3 In, out half3 Out, out half4 InstanceColor)
 {
 	Out = In;
-	InstanceColor = GetColor();
+	InstanceColor = GetFractalColor();
 }
