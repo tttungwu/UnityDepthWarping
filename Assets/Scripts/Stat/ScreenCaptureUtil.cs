93ccfbc93461ecdaf98fdd789ff51e491077cb30
tungwu <tungwuwt@gmail.com>
2025-03-26 22:22:50 +0800

    screenCapture

---
+++ Assets/Scripts/Stat/ScreenCaptureUtil.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Stat
+{
+    public class ScreenCaptureUtil : MonoBehaviour
+    {
+        private enum CaptureType
+        {
+            Reference,
+            Predict
+        };
+        
+        private int frameIndex = 0;
+        [SerializeField] private CaptureType captureType = CaptureType.Reference;
+        private string savePath = "Assets/Record/";
+        
+        void Update()
+        {
+            string filePathPrefix;
+            if (captureType == CaptureType.Reference) filePathPrefix = "/Reference/screen/frame_";
+            else filePathPrefix = "/Predict/screen/frame_";
+            string fileName = $"{savePath}{filePathPrefix}{frameIndex.ToString("D5")}.png";
+            ScreenCapture.CaptureScreenshot(fileName);
+            frameIndex++;
+            Debug.Log($"Saved frame to: {fileName}");
+        }
+    }
+}
