63f7cad74083db2563e0db57cada16b9b7146eb3
tungwu <tungwuwt@gmail.com>
2025-03-30 15:59:11 +0800

    property

--- Assets/Scripts/Core/IndirectDraw/HiZCulling.cs
+++ Assets/Scripts/Core/IndirectDraw/HiZCulling.cs
@@ -18,5 +18,23 @@ namespace Core.IndirectDraw
         private int _maxMipmapKernel;
         private int _computeVisibilityKernel;
         
+        private static readonly int FarClipPlaneShaderPropertyID = Shader.PropertyToID("FarClipPlane");
+        private static readonly int NearClipPlaneShaderPropertyID = Shader.PropertyToID("NearClipPlane");
+        private static readonly int ProjectionMatrixShaderPropertyID = Shader.PropertyToID("ProjectionMatrix");
+        private static readonly int PrevMipmapBufferPropertyID = Shader.PropertyToID("PrevMipmapBuffer");
+        private static readonly int CurMipmapBufferPropertyID = Shader.PropertyToID("CurMipmapBuffer");
+        private static readonly int MipmapWidthPropertyID = Shader.PropertyToID("MipmapWidth");
+        private static readonly int MipmapHeightPropertyID = Shader.PropertyToID("MipmapHeight");
+        private static readonly int ObjectNumPropertyID = Shader.PropertyToID("ObjectNum");
+        private static readonly int WidthPropertyID = Shader.PropertyToID("Width");
+        private static readonly int HeightPropertyID = Shader.PropertyToID("Height");
+        private static readonly int BoundsMinPropertyID = Shader.PropertyToID("BoundsMin");
+        private static readonly int BoundsMaxPropertyID = Shader.PropertyToID("BoundsMax");
+        private static readonly int CurrentProjectionViewMatrixPropertyID = Shader.PropertyToID("CurrentProjectionViewMatrix");
+        private static readonly int MipmapBufferPropertyID = Shader.PropertyToID("MipmapBuffer");
+        private static readonly int ModelMatrixBufferPropertyID = Shader.PropertyToID("ModelMatrixBuffer");
+        private static readonly int CullResultBufferPropertyID = Shader.PropertyToID("CullResultBuffer");
+        
+        
     }
 }
